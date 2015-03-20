70c41575dd8b69de1cf76c79cb3a309d1ec04e14
ExModding <exmodding@gmail.com>
2015-03-20 18:21:49 +0200

    v 1.1

--- balance.py
+++ balance.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import requests
+import sys
 from bs4 import BeautifulSoup
 
-
 URL = 'https://assa.intertelecom.ua/ru/login'
-LOGIN = '442216339'
-PASSWORD = 'LookS2014s'
 
-requests.packages.urllib3.disable_warnings()
-payload = {'phone': LOGIN, 'pass': PASSWORD}
-page = requests.post(URL, data=payload, )
+### Enable this in case of insecure connection error ###
+#requests.packages.urllib3.disable_warnings()
 
+
+if len(sys.argv) <= 1:
+    print('Error! Please specify login and password')
+    sys.exit(1)
+
+payload = {'phone': sys.argv[1], 'pass': sys.argv[2]}
+page = requests.post(URL, data=payload)
 tree = BeautifulSoup(page.text)
 tree.find_all('tbody')
+
+
 
 for tbody in tree.find_all('tbody'):
     for tr in tbody.find_all('tr'):
         for td in tr.find_all('td'):
             if u'Сальдо' in td:
-                print tr.text.split()[1]
-                break
+                result = tr.text.split()[1]
+print result
+
